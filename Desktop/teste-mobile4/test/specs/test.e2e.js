427609be65673113ac58548f181a8e982fa46559
Hiago G <123571165+hiagogil@users.noreply.github.com>
2023-12-07 21:07:21 -0300

    Update test.e2e.js
    Adicionado verificação.

--- Desktop/teste-mobile4/test/specs/test.e2e.js
+++ Desktop/teste-mobile4/test/specs/test.e2e.js
@@ -1,10 +1,16 @@
-
 describe('Acessar forms', () => {
     it('Acessando forms', async () => {
         await $('~Forms').click();
         await $('~text-input').setValue('test');
 
-    
+        expect(await $('~input-text-result').getText()).toBe('test');
+
+        await $('~Dropdown').click();
+
+        await seletor.SelectDrop()
+
+        await $('~button-Active').click()
+
 
     });
 });
